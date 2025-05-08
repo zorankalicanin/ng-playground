fc26dcefdf8dd6ee083dcc8ed3d214a96387b27e
zkalicanin <zoran.kalicanin@gmail.com>
2025-05-09 01:09:19 +0200

    Added left-sidebar and header

---
+++ my-proxima/src/app/src/app/components/left-sidebar/left-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-sidebar',
+  imports: [],
+  templateUrl: './left-sidebar.component.html',
+  styleUrl: './left-sidebar.component.scss'
+})
+export class LeftSidebarComponent {
+
+}
